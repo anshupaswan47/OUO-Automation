95cda8cfbdd955001bb4e10ea5de908c5a32f690
Anshu <149694024+anshupaswan47@users.noreply.github.com>
2024-05-19 15:21:08 +0530

    Update Anshu.py

--- Anshu.py
+++ Anshu.py
@@ -2,9 +2,11 @@ from itertools import count
 from multiprocessing.connection import wait
 from re import sub
 from selenium import webdriver 
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from time import sleep
+from selenium_stealth import stealth
 from random import randint, choice
 
 # urls = ['https://linkpays.in/Z7N6Vz25','https://linkpays.in/ALnKpCv','https://linkpays.in/AWptRg']
@@ -46,7 +48,26 @@ user_agent = choose_random_user_agent()
 options.add_argument(f"user-agent={user_agent}")
 options.add_argument("--headless=new")
 options.add_argument('--ignore-certificate-errors')
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option('useAutomationExtension', False)
+
 driver = webdriver.Chrome(options=options)
+driver.execute_cdp_cmd('Page.addScriptToEvaluateOnNewDocument', {
+    'source': '''
+        Object.defineProperty(navigator, 'webdriver', {
+          get: () => undefined
+        })
+    '''
+})
+
+stealth(driver,
+        languages=["en-US", "en"],
+        vendor="Google Inc.",
+        platform="Win32",
+        webgl_vendor="Intel Inc.",
+        renderer="Intel Iris OpenGL Engine",
+        fix_hairline=True,
+        )
 driver.get("chrome-extension://eppiocemhmnlbhjplcgkofciiegomcon/popup/index.html#/welcome-consent")
 driver.maximize_window()
 
@@ -65,12 +86,15 @@ def vpn(driver):
     driver.switch_to.window(driver.window_handles[0])
     clickby_xpath(driver,"/html/body/div/div/div[2]/div/div/div/button[2]")
     clickby_xpath(driver,"/html/body/div/div/div[3]/div[2]/div/div[1]/input")
+    driver.find_element(By.XPATH,value="/html/body/div/div/div[3]/div[2]/div/div[1]/input").send_keys("United States (USA)")
     driver.implicitly_wait(5)
     i=1
     clickby_xpath(driver,"/html/body/div/div/div[3]/div[2]/div/div[2]/div/ul/li["+str(i)+"]")
+    sleep(1)
     driver.switch_to.window(driver.window_handles[1])
     print(B+"VPN ACTIVATED"+YY)
     sleep(1)
+    # /html/body/div/div/div[3]/div[2]/div/div[2]/div/ul/li[81]
     
     start(driver)
 
@@ -84,6 +108,7 @@ def start(driver):
     process(driver)
 
 def clickby_id(driver, id, attempts=5):
+
     try:
         driver.find_element(By.ID, id).click()
         print(G+"ID Success"+C)
@@ -92,16 +117,15 @@ def clickby_id(driver, id, attempts=5):
         # clickby_id(driver, id, attempts-1) 
     
 def clickby_xpath(driver, path, attempts=5):
+    if attempts == 0:
+        driver.quit()
     try:
         driver.find_element(By.XPATH, path).click()
         print(G+"XPath Success"+C)
     except:
         print(RR+"XPath error"+C)
         sleep(1)
-        try:
-            clickby_xpath(driver, path, attempts-1)
-        except:
-            pass
+        clickby_xpath(driver, path, attempts-1)
 
 here= '/html/body/section/div/div/div/div/div/span[2]/a'
 here2 ='/html/body/section/div/div/div/div/div/span/a'
@@ -113,12 +137,15 @@ confirm = ''
 
 
 
+
+
+            
+            
 def process(driver):
     print(B+"PROCCESS STARTED "+YY)
     # waitfor(here) 
     sleep(3)  
     id="btn-main"
-    clickby_id(driver,id)
     clickby_xpath(driver,here)
     waitfor(textx)
     driver.find_element(by=By.XPATH,value=textx).send_keys("Hello")
@@ -133,9 +160,9 @@ def process(driver):
     else:
         clickby_xpath(driver,getlink)
     
-    sleep(4)
+    sleep(10)
     save_used_user_agent(user_agent)
     print("USER AGENT SAVED")
     driver.quit()
-
+    
 vpn(driver)
